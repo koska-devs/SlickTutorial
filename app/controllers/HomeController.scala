99dc1c99e9b21d2cee8da2ced6f838a8dd620073
Kenichiro Sone <1113146c@gmail.com>
2019-11-15 00:11:33 +0900

    addまで追加

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,23 +1,51 @@
 package controllers
 
+import dto.Tables._
 import javax.inject._
 import play.api._
-import play.api.mvc._
+import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfigProvider}
+import play.api.mvc.{Action, _}
+import slick.jdbc.PostgresProfile
+import slick.jdbc.PostgresProfile.api._
+
+import scala.concurrent.ExecutionContext
+import scala.util.Random
 
 
 @Singleton
-class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class HomeController @Inject()(protected val dbConfigProvider: DatabaseConfigProvider, cc: ControllerComponents)
+                              (implicit ec: ExecutionContext)
+  extends AbstractController(cc) with HasDatabaseConfigProvider[PostgresProfile] {
 
-  def index() = Action { implicit request: Request[AnyContent] =>
-    Ok("")
+  def index(): Action[AnyContent] = Action.async { implicit request =>
+    val action = User.result
+    db.run(action)
+      .map(
+        users => Ok(users.toList.mkString(","))
+      )
+      .recover{
+        case exception: Exception => InternalServerError(exception.getMessage)
+      }
   }
 
-  def explore() = Action { implicit request: Request[AnyContent] =>
-    Ok("")
+  def count(): Action[AnyContent] = Action.async {
+    val action = User.result
+    db.run(action)
+      .map(
+        users => Ok(users.size.toString)
+      )
+      .recover{
+        case exception: Exception => InternalServerError(exception.getMessage)
+      }
   }
 
-  def tutorial() = Action { implicit request: Request[AnyContent] =>
-    Ok("")
-  }
+  def add() = Action.async{
+    val action = User returning User.map(user => (user.id,user.name)) += UserRow(0, Random.alphanumeric.take(7).mkString)
 
+    db.run(action).map(
+      tuple=> Ok("ID:"+tuple._1.toString+"ユーザー名:"+tuple._2+"のユーザーが作成されました。")
+    ).recover{
+      case exception: Exception => InternalServerError(exception.getMessage)
+    }
+  }
 }
