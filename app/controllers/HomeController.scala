6a29863e4f7d0db2d0afacdfac38d5c0fd1b67dd
Kenichiro Sone <1113146c@gmail.com>
2019-12-29 12:38:38 +0900

    addMemoAndLabelメソッド追加

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -39,11 +39,26 @@ class HomeController @Inject()(protected val dbConfigProvider: DatabaseConfigPro
       }
   }
 
-  def add() = Action.async{
+  def add(): Action[AnyContent] = Action.async{
     val action = User returning User.map(user => (user.id,user.name)) += UserRow(0, Random.alphanumeric.take(7).mkString)
 
     db.run(action).map(
       tuple=> Ok("ID:"+tuple._1.toString+"ユーザー名:"+tuple._2+"のユーザーが作成されました。")
+    ).recover{
+      case exception: Exception => InternalServerError(exception.getMessage)
+    }
+  }
+
+  def addMemoAndLabel(userId:Int): Action[AnyContent] = Action.async{
+    val action=
+      for{
+        memo <-Memo returning Memo.map(memo => memo.id) into((memo,id)=> memo.copy(id = id)) += MemoRow(0,"todo:"+Random.alphanumeric.take(7).mkString, userId)
+        label <- Label returning Label.map(label =>label.id) into((label,id)=>label.copy(id=id)) += LabelRow(0,"label:"+Random.alphanumeric.take(7).mkString)
+        _ <- Labelmemo += LabelmemoRow(0,memo.id,label.id)
+      } yield (memo,label)
+
+    db.run(action.transactionally).map(
+      tuple=> Ok("ID:"+tuple._1.id.toString+" ユーザーID:"+tuple._1.userId.toString+" コンテンツ:"+tuple._1.content+"のメモが作成されました。"+"\n"+"ID:"+tuple._2.id.toString+" コンテンツ:"+tuple._2.name+"のラベルが作成されました。")
     ).recover{
       case exception: Exception => InternalServerError(exception.getMessage)
     }
